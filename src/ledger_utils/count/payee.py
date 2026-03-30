e2680d5308fa89f5988724b38393371b93a84224
Tateishi Tadatoshi <ishio39@gmail.com>
2026-03-30 22:54:31 +0900

    payee の出現件数カウント

--- src/ledger_utils/count/payee.py
+++ src/ledger_utils/count/payee.py
@@ -1,13 +1,48 @@
 import re
 from pathlib import Path
 
+HEADER_RE = re.compile(r"^\d")
 
-def count(path: Path, text: str) -> dict[str, int]:
-    print(f"count: {path}")
+HEADER_PARSE_RE = re.compile(
+    r"""^
+    (?P<date>\d{4}-\d{2}-\d{2})
+    (?:
+        \s+
+        (?:(?P<flag>[*!])\s+)?     # optional * or !
+        (?P<payee>.*?)
+    )?
+    (?P<comment>\s*;\s*.*)?       # optional trailing comment ; ...
+    \s*$
+    """,
+    re.VERBOSE,
+)
 
-    result = dict()
 
-    result["abc"] = 1
-    result["def"] = 2
+def counter(text: str) -> dict[str, int]:
+    """
+    {PAYEE: 出現数} の辞書を返す
+    """
 
-    return result
+    count_dict = dict()
+
+    lines = text.splitlines()
+
+    for line in lines:
+        m_header = re.match(HEADER_PARSE_RE, line)
+        if m_header is None:
+            continue
+
+        payee = m_header.group("payee")
+        count_dict[payee] = count_dict.get(payee, 0) + 1
+
+    return count_dict
+
+
+def count(path: Path) -> dict[str, int]:
+    """
+    ファイル path に出現するpayeeの回数を返す
+    """
+
+    text = path.read_text()
+
+    return counter(text)
--- src/ledger_utils/typer_main.py
+++ src/ledger_utils/typer_main.py
@@ -4,8 +4,16 @@ import typer
 
 from ledger_utils.count import payee_count
 
+from collections import Counter
+
 app = typer.Typer()
 
+def iter_ledger_files(root:Path):
+    yield from (p for p in root.rglob("*.ledger") if p.is_file())
+
+def print_result(total: dict[str, int]):
+    for key, value in sorted(total.items(), key=lambda x: (x[1], x[0])):
+        print(f"count: {value:5d}, payee: {key}")
 
 @app.command()
 def hello():
@@ -14,8 +22,20 @@ def hello():
 
 @app.command()
 def count(path: Path):
-    print(path)
-    print(payee_count(path, "abc"))
+    if not path.is_dir():
+        print(path)
+        data = payee_count(path)
+
+        for key, value in sorted(data.items(), key=lambda x: (x[1], x[0])):
+            print(f"count: {value:3d}, payee: {key}")
+    else:
+        total = Counter()
+
+        for file in iter_ledger_files(path):
+            data = payee_count(file)
+            total.update(data)
+
+        print_result(total)
 
 
 def main():
